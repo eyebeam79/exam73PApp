2929a6cfa36b0b556efe54dca5c9a5d26c3ba40a
SDT1 <SDT1@C3-18ui-MacBook-Pro.local>
2014-01-02 13:13:49 +0900

    Initial Commit

---
+++ exam73PApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  exam73PApp
+//
+//  Created by SDT1 on 2014. 1. 2..
+//  Copyright (c) 2014년 SDT1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ exam73PApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  exam73PApp
+//
+//  Created by SDT1 on 2014. 1. 2..
+//  Copyright (c) 2014년 SDT1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
