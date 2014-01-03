d204e770427f9349d8e21b894837b237cf15e999
SDT1 <eyebeam79@naver.com>
2014-01-03 14:00:49 +0900

    포토 갤러리

--- exam73PApp/ViewController.m
+++ exam73PApp/ViewController.m
@@ -9,15 +9,93 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIImage *image1;
+    UIImage *image2;
+    UIImage *image3;
+    
+    int index;
+    
+    __weak IBOutlet UIButton *rightBtn;
+    __weak IBOutlet UIButton *leftBtn;
+}
+@property (strong, nonatomic) IBOutlet UIImageView *autoImage;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)rightClick:(id)sender
+{
+    index++;
+    
+    if(index>3) index = 3;
+    
+    switch (index) {
+        case 1:
+            self.autoImage.image = image1;
+            break;
+            
+        case 2:
+            self.autoImage.image = image2;
+            leftBtn.enabled = YES;
+            break;
+            
+        case 3:
+            self.autoImage.image = image3;
+            rightBtn.enabled = NO;
+            
+            break;
+    }
+    
+}
+
+
+- (IBAction)leftClick:(id)sender
+{
+    index--;
+    
+    if(!index) index = 1;
+    
+    switch (index) {
+        case 1:
+            self.autoImage.image = image1;
+            leftBtn.enabled = NO;
+            
+            break;
+            
+        case 2:
+            self.autoImage.image = image2;
+            rightBtn.enabled = YES;
+            break;
+            
+        case 3:
+            self.autoImage.image = image3;
+            break;
+    }
+
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    index = 1;
+    leftBtn.enabled = NO;
+	
+    NSString *imagePath1 = [[NSBundle mainBundle] pathForResource:@"auto1" ofType:@"jpg"];
+    NSString *imagePath2 = [[NSBundle mainBundle] pathForResource:@"auto2" ofType:@"jpg"];
+    NSString *imagePath3 = [[NSBundle mainBundle] pathForResource:@"auto3" ofType:@"jpg"];
+    
+    image1 = [UIImage imageWithContentsOfFile:imagePath1];
+    image2 = [UIImage imageWithContentsOfFile:imagePath2];
+    image3 = [UIImage imageWithContentsOfFile:imagePath3];
+    
+    
+    self.autoImage.image = image1;
+
+    
 }
 
 - (void)didReceiveMemoryWarning
